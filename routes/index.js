9425c19bc77e3a83fdb62ca437633c42e17606de
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-07-17 15:39:54 -0400

    Added /cluster route

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Red Hat Tech Exchange', clusterName, imageTag });
 });
 
+/* Return Cluster Name. */
+router.get('/cluster', function(req, res, next) {
+  if (process.env.CLUSTER_NAME) {
+    clusterName = process.env.CLUSTER_NAME;
+  }
+  if (process.env.IMAGE_TAG) {
+    imageTag = process.env.IMAGE_TAG;
+  }
+  res.contentType('application/text');
+  res.send(clusterName+"\n");
+});
+
 module.exports = router;
