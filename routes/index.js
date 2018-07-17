98ddaaecb71bb35c9a3210467d0fc98a3241ce97
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-07-17 14:47:58 -0400

    Added ImageTag to output

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var clusterName = 'Unknown Cluster'
+var imageTag    = 'Undefined'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (process.env.CLUSTER_NAME) {
     clusterName = process.env.CLUSTER_NAME;
   }
+  if (process.env.IMAGE_TAG) {
+    imageTag = process.env.IMAGE_TAG;
+  }
 
-  res.render('index', { title: 'Red Hat Tech Exchange', clusterName });
+  res.render('index', { title: 'Red Hat Tech Exchange', clusterName, imageTag });
 });
 
 module.exports = router;
